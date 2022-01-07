3e1202845675bc21834c1b98f6902eb29ac358c6
Даня <daniakutsev1488@gmail.com>
2022-01-07 16:13:23 +0300

    router

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,15 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ["plugin:vue/vue3-essential","plugin:prettier/recommended", "eslint:recommended", "@vue/prettier"],
+  extends: [  "@vue/eslint-config-cli-plugin-babel/preset","@vue/cli-plugin-babel/preset","eslint:recommended","plugin:prettier/recommended", "@vue/prettier"],
   parserOptions: {
     parser: "babel-eslint"
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "vue/block-lang": "error",
+    "vue/html-indent": "error",
+    'vue/script-setup-uses-vars': 'error',
   }
 };
