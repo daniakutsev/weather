52bc7b9ac72f67000ce44eeb3a390245ddc80f67
Даня <daniakutsev1488@gmail.com>
2022-01-26 01:34:46 +0300

    end

--- src/components/store/cardModule.js
+++ src/components/store/cardModule.js
@@ -3,31 +3,27 @@ import axios from "axios";
 export const cardModule = {
   state: () => ({
     cards: [],
-    point: ";",
     countId: 0,
 
-    jsonCookiesCards: []
   }),
   getters: {
     getCards(state) {
       return state.cards;
-    }
-
+    },
   },
   mutations: {
     setCards(state) {
       state.cards = this.cards;
-    }
+    },
   },
   actions: {
-
     removeCard({ state }, card) {
-      //this.cards = this.cards.filter((c) => c.id !== card.id);
-
-      if (card.id === 0) {
-        state.cards.splice(card.id, 1);
-      } else {
-        state.cards.splice(card.id - 1, 1);
+      for (let i = 0; i < state.cards.length; i++) {
+        if (state.cards[i].id === card.id) {
+          state.cards.splice(i, 1);
+          let newCookie = JSON.stringify(state.cards);
+          document.cookie = `cities=${newCookie}`;
+        }
       }
     },
     async createCard({ state }, card) {
@@ -42,68 +38,33 @@ export const cardModule = {
         card.country = response.data.sys.country;
         card.id = state.countId++;
 
-        // let hasCookie = document.cookie.match(/cities=(.+?)(;|$)/);
-        // if (hasCookie == null) {
-        //   let tempData = [];
-        //   tempData.push(state.cards[card.id]);
-        //   document.cookie = "cities=" + JSON.stringify(tempData);
-        // }
-        // if (hasCookie != null) {
-        //   let arr = JSON.parse(hasCookie[1]);
-        //   arr.push(state.cards[card.id]);
-        //   document.cookie = "cities=" + JSON.stringify(arr);
-        // }
-
-
-        if (document.cookie === "") {
-          // state.jsonCookiesCards.push(JSON.stringify(state.cards[card.id]));
-          // document.cookie = state.jsonCookiesCards[state.cards[card.id]];let tempData = [];
-
-          document.cookie = JSON.stringify(state.cards);
-          console.log(document.cookie);
+        let hasCookie = document.cookie.match(/cities=(.+?)(;|$)/);
+        if (hasCookie == null) {
+          let tempData = [];
+          tempData.push(state.cards[card.id]);
+          document.cookie = "cities=" + JSON.stringify(tempData);
+        } else {
+          let arr = JSON.parse(hasCookie[1]);
+          arr.push(state.cards[card.id]);
+          console.log(state.cards[card.id])
+          document.cookie = "cities=" + JSON.stringify(arr);
         }
-        if (document.cookie != "") {
-          state.jsonCookiesCards = JSON.parse(document.cookie);
-          state.jsonCookiesCards.push(state.cards[card.id]);
-          document.cookie = JSON.stringify(state.jsonCookiesCards);
-        }
-
+        console.log(document.cookie);
 
       } catch (e) {
         console.log(e);
       }
     },
 
-    loadCookie({ state },card) {
-      try {
+    loadCookie({ state }, card) {
+      let hasCookie = document.cookie.match(/cities=(.+?)(;|$)/);
 
-        state.cards[card.id] = JSON.parse(document.cookie);
-        console.log(state.cards);
-      } catch (e) {
-        console.log(e);
+      if (hasCookie != null) {
+        card = JSON.parse(hasCookie[1]);
+        state.cards = card;
+        state.countId = state.cards.length;
       }
 
-      // let hasCookie = document.cookie;
-      // if (JSON.parse(hasCookie) != null) {
-      //
-      //   console.log(hasCookie);
-      // }
-      //   // card.temperature = hasCookie[state.countId].temperature;
-      //   // card.cityName = hasCookie[state.countId].cityName;
-      //   // card.feelsLike = hasCookie[state.countId].feelsLike;
-      //   // card.windSpeed = hasCookie[state.countId].windSpeed;
-      //   // card.country = hasCookie[state.countId].country;
-      //   // card.id = hasCookie[state.countId].id;
-      //   state.cards.push(card);
-
-
-      // if (document.cookie != "") {
-      //   let buff = document.cookie.match(/(.+?)(;|$)/);
-      //   buff.slice(0, -1);
-      //   console.log(buff);
-      //   card = JSON.parse(buff);
-      //   state.cards.push(card);
-      // }
     },
     async updateWeather({ state }, card) {
       console.log(state.cards);
@@ -118,6 +79,6 @@ export const cardModule = {
       } catch (e) {
         console.log(e);
       }
-    }
-  }
+    },
+  },
 };
